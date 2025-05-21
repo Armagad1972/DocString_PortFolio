d9ec4f818d2de2588682de782c60fa2caf183bab
Armagad1972 <arnaud.lanthony@jadellys.fr>
2025-05-21 22:31:28 +0200

    Commit complete

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from accounts.models import Societe, Magasin, Produit
+from accounts.models import Societe, Magasin, Produit, Stock, Mouvements, JadUser
 
 # Register your models here.
+admin.site.register(JadUser)
 admin.site.register(Societe)
 admin.site.register(Magasin)
 admin.site.register(Produit)
+admin.site.register(Stock)
+admin.site.register(Mouvements)
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.core.validators import MinValueValidator
 from django.db import models
